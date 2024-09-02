d1478bbb43c57ec3c01bb4e8c0a8f9da7042337c
WintiS <kviteksima@seznam.cz>
2024-09-02 16:43:47 +0200

    fix android

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -221,11 +221,7 @@ export default component$(() => {
       </div>
 
       <div class={"pb-24"}>
-        <div
-          class={
-            "flex -translate-y-16 items-center justify-center md:translate-y-0"
-          }
-        >
+        <div class={"flex items-center justify-center md:translate-y-0"}>
           <SvgWaveTop />
         </div>
         <div
