baf874b6d204b494dc8ba0ab2257bc0f52ee92b8
WintiS <kviteksima@seznam.cz>
2024-10-13 21:10:32 +0200

    cta
    chci webovku cta button

--- src/components/ui/headerbutton.tsx
+++ src/components/ui/headerbutton.tsx
@@ -7,11 +7,21 @@ export const HeaderButton = component$(() => {
       <a
         href="#projects"
         class={
-          " inline-block items-center gap-1 rounded-[5px] border-2 border-primary bg-transparent px-5  py-3 transition-colors hover:bg-primary"
+          "mb-4 inline-block items-center gap-1 rounded-[5px] border-2 border-primary bg-transparent px-5  py-3 transition-colors hover:bg-primary"
         }
       >
         <p class={"flex items-center gap-2"}>
           Portfolio projektů <LuArrowRight />
+        </p>
+      </a>
+      <a
+        href="#form"
+        class={
+          "inline-block  items-center gap-1 rounded-[5px] border-2 border-white bg-transparent px-5 py-3  transition-colors hover:border-primary hover:bg-primary"
+        }
+      >
+        <p class={"flex items-center gap-2"}>
+          Chci webovku <LuArrowRight />
         </p>
       </a>
     </div>
